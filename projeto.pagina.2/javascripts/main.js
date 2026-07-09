ff7d359d56d9f5112aadb44412f1395cb30eaf37
Belatrix <beatrizsilvadeoliveira822@gmail.com>
2026-07-09 19:39:15 +0000

    primeiro.javascript.no.projeto

--- projeto.pagina.2/javascripts/main.js
+++ projeto.pagina.2/javascripts/main.js
@@ -1,1 +1,14 @@
-console.log("ola mundo")
+const textoDousuario = document.getElementById("perg")
+const botaoPronto = document.getElementById("pronto")
+const caixaDogato = document.querySelector(".box")
+
+console.log(textoDousuario)
+console.log(botaoPronto)
+console.log(caixaDogato)
+
+botaoPronto.addEventListener('click', function(){
+    let codigoDigitado = textoDousuario.value
+    console.log(codigoDigitado)
+})
+
+
