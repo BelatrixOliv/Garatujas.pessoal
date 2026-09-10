6a8ebe356f34efdf221d2f44b4a89ff0bef68561
beatriz <beatrizsilvadeoliveira822@gmail.com>
2026-09-10 14:38:43 -0300

    gab-alterações

--- gab-cod/Projeto.tech-main/script.js
+++ gab-cod/Projeto.tech-main/script.js
@@ -9,7 +9,7 @@ cliquesm = 0;
 cliquesd = 0;
 cliquesf++;
 if(cliquesf == 2){
-    window.location.href="https://opulent-space-cod-pjx7rr4ggv55h6wvv-5518.app.github.dev/projeto.pagina.2/"
+    window.location.href="http://127.0.0.1:5518/projeto.pagina.2/index.html"
     cliquesf=0;
 }
 }
